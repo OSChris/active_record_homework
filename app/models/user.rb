02e76435953e1526ae99845b06d619a31e8a8821
Chris Yammine <chris.yammine@hotmail.com>
2014-05-27 17:01:43 -0700

    add created_after scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   # scope: :full_search, lambda { |search_item| where(["first_name LIKE ? OR last_name LIKE ? OR email LIKE ?", "%#{search_term}%", "%#{search_term}%", "%#{search_term}%"]) }
   # scope: :date_range, -> { |date1, date2| where(["created_at > ?", date1]).where("created_at < ?", date2) }
   # scope: :exact_search, -> { |search_item| where(["first_name LIKE ? OR last_name LIKE ? OR email LIKE ?", search_item, search_item, search_item]) }
-
+  scope :created_after, lambda { |t| where(["created_at > ?", t]) }
 
   def self.date_range(date1, date2)
     where(["created_at > ?", date1]).where("created_at < ?", date2) 
