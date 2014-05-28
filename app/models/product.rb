62e97a4b62782a121723c7d7c64eac05d11eb552
Chris Yammine <chris.yammine@hotmail.com>
2014-05-27 19:17:42 -0700

    Add log message before destroy on product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,10 @@ class Product < ActiveRecord::Base
 
   validates :name, presence: true
   validates :price, numericality: { only_integer: true, greater_than: 0, less_than: 1000 }
-  
+
   scope :grab_new_products, lambda { where(["updated_at > ?", 60.minutes.ago]) }
+
+  before_destroy :destroy_logger 
 
   def self.between_one_and_three_hundred
     where(["price > 100"]).where(["price < 300"])
@@ -11,6 +13,12 @@ class Product < ActiveRecord::Base
 
   def self.on_sale
     grab_new_products.where.not(["price = sale_price"]).order("sale_price ASC").limit(3)
+  end
+
+private
+  
+  def destroy_logger
+    logger.info "#{self} is being destroyed."
   end
 
 end
