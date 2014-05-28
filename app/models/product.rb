dc52683fb59bdc10f3a417a858a66d2b0ba80302
Chris Yammine <chris.yammine@hotmail.com>
2014-05-27 20:06:16 -0700

    add validation for sale_price

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,12 @@
 class Product < ActiveRecord::Base
-
-  validates :name, presence: true
+#validates that the name parameter is present, unique, and doesn't contain the reserved words
+  validates :name, presence: true, uniqueness: true, exclusion: { in: %w(Apple Microsoft Sony)}
+#validates that the price is a number thats between 0 and 1000
   validates :price, numericality: { only_integer: true, greater_than: 0, less_than: 1000 }
 
   scope :grab_new_products, lambda { where(["updated_at > ?", 60.minutes.ago]) }
+
+  validate :sale_price_cannot_be_higher_than_price
 
   before_destroy :destroy_logger 
 
@@ -19,6 +22,13 @@ private
   
   def destroy_logger
     logger.info "#{self} is about to be deleted."
+  end
+
+  def sale_price_cannot_be_higher_than_price
+    self.sale_price ||= self.price
+    if self.sale_price > self.price
+      errors.add(:sale_price, "can't be higher than price.")
+    end
   end
 
 end
